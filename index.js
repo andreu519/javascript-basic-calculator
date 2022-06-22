44c03be62fe95c88e2e2a6f665f169f97e396719
Andreu <62018304+andreu519@users.noreply.github.com>
2022-06-22 12:57:35 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+// take the operator from the user through prompt box in JavaScript.  
+const operator = prompt('Enter operator to perform the calculation ( either +, -, * or / ): ');  
+  
+// accept the number from the user through prompt box  
+const number1 = parseFloat(prompt ('Enter the first number: '));  
+const number2 = parseFloat(prompt ('Enter the second number: '));  
+  
+let result; // declaration of the variable.  
+  
+
+if (operator == '+') { // use + (addition) operator to add two numbers  
+    result = number1 + number2;  
+}  
+else if (operator == '-') { // use -  (subtraction) operator to subtract two numbers  
+    result = number1 - number2;  
+}  
+else if (operator == '*') { // use * (multiplication) operator to multiply two numbers  
+    result = number1 * number2;  
+}  
+else {  
+    result = number1 / number2; // use / (division) operator to divide two numbers  
+}  
+  
+// display the result of the Calculator  
+window.alert(" Result is" + result);  
